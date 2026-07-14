31ce0aaf788a82ceddb3f2267e62e067adc4302b
YgtEfeDvcglu <deveciogluyigitefe@gmail.com>
2026-07-15 00:57:56 +0300

    Update Sevgilime Site.py

--- Sevgilime Site.py
+++ Sevgilime Site.py
@@ -959,6 +959,7 @@ def render_album() -> None:
             position: absolute;
             top: 0; left: 0; right: 0; bottom: 0;
             display: flex;
+            z-index: 20;
             opacity: 0;
             transform: scale(0.96) translateY(10px);
             transition: all 0.6s cubic-bezier(0.4, 0, 0.2, 1);
